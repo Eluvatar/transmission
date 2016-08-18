a6b2985f67797cc3d89b8a1890f2b30752e4f2ce
Eluvatar <drstadler@gmail.com>
2016-08-18 19:14:09 -0400

    add use of catchup to transmission_log

--- transmission_log.py
+++ transmission_log.py
@@ -33,9 +33,18 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Log happenings")
 parser.add_argument('-o','--output', metavar='FILE', help='a file to log XML events to')
-parser.add_argument('-p','--port', metavar='PORT', help='a port number on which to talk to Transmission (default=6261)')
+parser.add_argument('-p','--port', metavar='PORT', help='a port number on which to talk to Transmission (default=6261)', default=6261)
+parser.add_argument('-q','--quiet', action='store_true', help='whether to omit printing to stdout')
+parser.add_argument('-s','--start', metavar='EVENTID', help='event ID to start from', default=None, type=int)
+parser.add_argument('-u','--user', metavar='NATION_OR_USER_NAME', help='name to use in user agent string')
+parser.add_argument('-e','--event_pattern', metavar='PATTERN', help='a pattern on which to match (default any)', default='.*')
+parser.add_argument('-a','--auto', action='store_true', help='whether to start from the last happening in the output file')
 
 args = parser.parse_args()
+
+print args
+
+reception.api.user_agent = "Transmission v0.1.0 ({0})".format(args.user)
 
 if args.output:
     out = open(args.output,"a")
@@ -49,9 +58,18 @@ def xml(event):
     text.text = ET.CDATA(event.text)
     return x
 
-@reception.subscribe(".*",port=6263)
+if args.auto:
+    with open(args.output,'r') as xmlfile:
+        xmlfile.seek(-2,2)
+        while xmlfile.read(1) != b"\n":
+            xmlfile.seek(-2,1)
+        last = xmlfile.readline()
+        args.start = int(ET.fromstring(last).get("id"))
+
+@reception.subscribe(pattern=args.event_pattern,port=args.port,from_event_id=args.start)
 def print_text_and_log_xml(e):
-    print e.text
+    if not args.quiet:
+        print e.text
     if args.output:
         print >> out, ET.tostring(xml(e))
 
